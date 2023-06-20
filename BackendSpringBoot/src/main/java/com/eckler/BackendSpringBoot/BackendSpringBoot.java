fda30c3bda5d8163c007174b8c10982a824e346b
Dennis.Eckler <eckler.d95@gmail.com>
2023-06-20 14:27:08 +0200

    initial push with backend

---
+++ BackendSpringBoot/src/main/java/com/eckler/BackendSpringBoot/BackendSpringBoot.java
@@ -0,0 +1,12 @@
+package com.eckler.BackendSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBoot {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringBoot.class, args);
+		System.out.println("Herllo");
+	}
+}
---
+++ BackendSpringBoot/src/test/java/com/eckler/BackendSpringBoot/BackendSpringBootTests.java
@@ -0,0 +1,13 @@
+package com.eckler.BackendSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
